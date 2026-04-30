75fdc3da060d3762dbb38d74ebe49adc20c16065
Kunjan1810 <kunjantrivedi2000@gmail.com>
2026-04-30 16:08:14 +0530

    changes in setting file for database

--- my_dummy_site/settings.py
+++ my_dummy_site/settings.py
@@ -72,11 +72,22 @@ WSGI_APPLICATION = 'my_dummy_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Aa line Render par DATABASE_URL shodhashe, jo na male to local sqlite vaparshe
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
